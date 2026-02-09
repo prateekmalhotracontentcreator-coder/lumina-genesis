f825aeedf151ffd923fcfe00f4a652a6d2ed527b
prateekmalhotracontentcreator-coder <prateekmalhotra.contentcreator@gmail.com>
2026-02-09 06:12:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -20,6 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': './'
-    }
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   }
 });
